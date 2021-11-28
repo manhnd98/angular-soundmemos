16e10ee1a894d1c40b700104eb652e1356fbe80f
Nguyễn Đức Mạnh <manhnd.jin@gmail.com>
2021-11-28 13:01:08 +0700

    refactor(structure): add folder structure and library

--- apps/soundmemos/src/app/app.module.ts
+++ apps/soundmemos/src/app/app.module.ts
@@ -3,10 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    StoreModule.forRoot(
+      {},
+      {
+        metaReducers: !environment.production ? [] : [],
+        runtimeChecks: {
+          strictActionImmutability: true,
+          strictStateImmutability: true,
+        },
+      }
+    ),
+    EffectsModule.forRoot([]),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ libs/web/utils/src/lib/browser.ts
@@ -0,0 +1,24 @@
+interface DetectBrowser {
+  browser: string | null;
+  version: number | null;
+  minVersion: number | null;
+}
+
+/**
+ * get current browser information
+ * @returns {result.browser}: browser name `chrome`, `firefox`, etc.
+ * @returns {result.version}: current version number
+ * @returns {result.minVersion}: minimum version
+ */
+export function detectBrowser() {
+  const result: DetectBrowser = {
+    browser: null,
+    version: null,
+    minVersion: null,
+  };
+
+  if (typeof window === 'undefined' || !window.navigator) {
+    result.browser = 'Not a supported browser.';
+    return result;
+  }
+}
